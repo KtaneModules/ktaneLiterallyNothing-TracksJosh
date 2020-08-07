7807eaf7ddeecfddd50a1631226bba2d79997daa
TracksJosh <arcticwolf1526@gmail.com>
2020-08-07 01:36:39 -0500

    Initial
    Initial

---
+++ Assets/literallyNothingScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleNeedyModule : MonoBehaviour
+{
+
+
+    void Awake()
+    {
+        GetComponent<KMNeedyModule>().OnNeedyActivation += OnNeedyActivation;
+        GetComponent<KMNeedyModule>().OnNeedyDeactivation += OnNeedyDeactivation;
+        GetComponent<KMNeedyModule>().OnTimerExpired += OnTimerExpired;
+    }
+
+    protected bool Solve()
+    {
+        GetComponent<KMNeedyModule>().OnPass();
+
+        return false;
+    }
+
+    protected void OnNeedyActivation()
+    {
+
+    }
+
+    protected void OnNeedyDeactivation()
+    {
+
+    }
+
+    protected void OnTimerExpired()
+    {
+
+    }
+
+}
