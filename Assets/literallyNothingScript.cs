f61cd051edac2ec31721fd783c4a47e408d5a47e
TracksJosh <68456891+TracksJosh@users.noreply.github.com>
2020-09-09 17:16:40 -0500

    Updated literallyNothingScript again.
    Made it actually work in Twitch Plays.

--- Assets/literallyNothingScript.cs
+++ Assets/literallyNothingScript.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using System.Collections;
+using System;
 
-public class LiterallyNothing : MonoBehaviour
+public class literallyNothingScript : MonoBehaviour
 {
+    public KMAudio Audio;
+
+
     void Awake()
     {
-
+        Audio = GetComponent<KMAudio>();
     }
 
     protected bool Solve()
@@ -15,9 +19,20 @@ public class LiterallyNothing : MonoBehaviour
 
     private readonly string TwitchHelpMessage = @"Use !{0} () to do literally nothing";
 
-    IEnumerator ProcessTwitchCommand(string Command)
+    IEnumerator ProcessTwitchCommand(string command)
     {
-        yield return null;
-        yield return "awardpoints -1";
+        string[] twitchArena = command.Trim().ToLowerInvariant().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        if (twitchArena.Length > 1 && (twitchArena[0] != "claim"))
+        {
+            if (twitchArena.Length > 1 && (twitchArena[0] != "literallyNothing"))
+            {
+                yield return null;
+                //yield return "awardpoints -1";
+            }
+            else
+            {
+                Audio.PlaySoundAtTransform("easterEgg1", transform);
+            }
+        }
     }
 }
